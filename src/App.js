89c89b3dfd1ebbe7d2a862e5190aea29f32585cd
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-10-04 05:53:25 -0400

    created 2nd repo to test stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+// import {Link, BrowserRouter as Router} from 'react-router-dom;'
+// import logo from './logo.svg';
+// import api from "./components/Home"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+
+import Edith from "./about/Edith";
+import Samim from "./about/Samim";
+import Zach from "./about/Zach";
+import Jaden from "./about/Jaden";
+
+function About ( ) {
+    return (
+      <div className="Group">
+        <span> </span>
+        <Edith />
+        <Jaden/>
+        <Samim />
+        <Zach />
+      </div>
+    );
+  }
+  
+export default About;
